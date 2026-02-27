7d7ed4d22dae023b8bd0d9679f391f76066fa02d
Elias Eduardo Liranza Pérez <146482192+eliaselp@users.noreply.github.com>
2026-02-26 22:19:16 -0600

    Fix regex pattern for room websocket URL

--- rooms/routing.py
+++ rooms/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/room/(?P<room_name>\w+)/(?P<token>[^/]+)/$', consumers.RoomConsumer.as_asgi()),
+    re_path(r'ws/room/(?P<room_name>[^/]+)/(?P<token>[^/]+)/$', consumers.RoomConsumer.as_asgi()),
 ]
